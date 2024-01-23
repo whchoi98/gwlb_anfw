0560892ab66dc8daa19fd5986f52f5113c84fd6a
whchoi98 <whchoi98@gmail.com>
2024-01-23 23:25:55 +0900

    edit - changed region in ANFW

--- anfw/anfw_ec2_shell.sh
+++ anfw/anfw_ec2_shell.sh
@@ -1,40 +1,40 @@
 #!/bin/bash
 # command ./anfw_ec2_shell.sh
 
-aws ec2 describe-instances --region ap-northeast-1 --filters 'Name=tag:Name,Values=ANFW-VPC01-Private-A-10.1.21.101' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId'
-aws ec2 describe-instances --region ap-northeast-1 --filters 'Name=tag:Name,Values=ANFW-VPC01-Private-A-10.1.21.102' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId'
-aws ec2 describe-instances --region ap-northeast-1 --filters 'Name=tag:Name,Values=ANFW-VPC01-Private-B-10.1.22.101' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId'
-aws ec2 describe-instances --region ap-northeast-1 --filters 'Name=tag:Name,Values=ANFW-VPC01-Private-B-10.1.22.102' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId'
-export ANFW_VPC01_21_101=$(aws ec2 describe-instances --region ap-northeast-1 --filters 'Name=tag:Name,Values=ANFW-VPC01-Private-A-10.1.21.101' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId')
-export ANFW_VPC01_21_102=$(aws ec2 describe-instances --region ap-northeast-1 --filters 'Name=tag:Name,Values=ANFW-VPC01-Private-A-10.1.21.102' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId')
-export ANFW_VPC01_22_101=$(aws ec2 describe-instances --region ap-northeast-1 --filters 'Name=tag:Name,Values=ANFW-VPC01-Private-B-10.1.22.101' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId')
-export ANFW_VPC01_22_102=$(aws ec2 describe-instances --region ap-northeast-1 --filters 'Name=tag:Name,Values=ANFW-VPC01-Private-B-10.1.22.102' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId')
+aws ec2 describe-instances --region ${AWS_REGION} --filters 'Name=tag:Name,Values=ANFW-VPC01-Private-A-10.1.21.101' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId'
+aws ec2 describe-instances --region ${AWS_REGION} --filters 'Name=tag:Name,Values=ANFW-VPC01-Private-A-10.1.21.102' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId'
+aws ec2 describe-instances --region ${AWS_REGION} --filters 'Name=tag:Name,Values=ANFW-VPC01-Private-B-10.1.22.101' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId'
+aws ec2 describe-instances --region ${AWS_REGION} --filters 'Name=tag:Name,Values=ANFW-VPC01-Private-B-10.1.22.102' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId'
+export ANFW_VPC01_21_101=$(aws ec2 describe-instances --region ${AWS_REGION} --filters 'Name=tag:Name,Values=ANFW-VPC01-Private-A-10.1.21.101' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId')
+export ANFW_VPC01_21_102=$(aws ec2 describe-instances --region ${AWS_REGION} --filters 'Name=tag:Name,Values=ANFW-VPC01-Private-A-10.1.21.102' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId')
+export ANFW_VPC01_22_101=$(aws ec2 describe-instances --region ${AWS_REGION} --filters 'Name=tag:Name,Values=ANFW-VPC01-Private-B-10.1.22.101' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId')
+export ANFW_VPC01_22_102=$(aws ec2 describe-instances --region ${AWS_REGION} --filters 'Name=tag:Name,Values=ANFW-VPC01-Private-B-10.1.22.102' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId')
 echo "export ANFW_VPC01_21_101=${ANFW_VPC01_21_101}"| tee -a ~/.bash_profile
 echo "export ANFW_VPC01_21_102=${ANFW_VPC01_21_102}"| tee -a ~/.bash_profile
 echo "export ANFW_VPC01_22_101=${ANFW_VPC01_22_101}"| tee -a ~/.bash_profile
 echo "export ANFW_VPC01_22_102=${ANFW_VPC01_22_102}"| tee -a ~/.bash_profile
 
-aws ec2 describe-instances --region ap-northeast-1 --filters 'Name=tag:Name,Values=ANFW-VPC02-Private-A-10.2.21.101' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId'
-aws ec2 describe-instances --region ap-northeast-1 --filters 'Name=tag:Name,Values=ANFW-VPC02-Private-A-10.2.21.102' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId'
-aws ec2 describe-instances --region ap-northeast-1 --filters 'Name=tag:Name,Values=ANFW-VPC02-Private-B-10.2.22.101' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId'
-aws ec2 describe-instances --region ap-northeast-1 --filters 'Name=tag:Name,Values=ANFW-VPC02-Private-B-10.2.22.102' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId'
-export ANFW_VPC02_21_101=$(aws ec2 describe-instances --region ap-northeast-1 --filters 'Name=tag:Name,Values=ANFW-VPC02-Private-A-10.2.21.101' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId')
-export ANFW_VPC02_21_102=$(aws ec2 describe-instances --region ap-northeast-1 --filters 'Name=tag:Name,Values=ANFW-VPC02-Private-A-10.2.21.102' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId')
-export ANFW_VPC02_22_101=$(aws ec2 describe-instances --region ap-northeast-1 --filters 'Name=tag:Name,Values=ANFW-VPC02-Private-B-10.2.22.101' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId')
-export ANFW_VPC02_22_102=$(aws ec2 describe-instances --region ap-northeast-1 --filters 'Name=tag:Name,Values=ANFW-VPC02-Private-B-10.2.22.102' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId')
+aws ec2 describe-instances --region ${AWS_REGION} --filters 'Name=tag:Name,Values=ANFW-VPC02-Private-A-10.2.21.101' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId'
+aws ec2 describe-instances --region ${AWS_REGION} --filters 'Name=tag:Name,Values=ANFW-VPC02-Private-A-10.2.21.102' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId'
+aws ec2 describe-instances --region ${AWS_REGION} --filters 'Name=tag:Name,Values=ANFW-VPC02-Private-B-10.2.22.101' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId'
+aws ec2 describe-instances --region ${AWS_REGION} --filters 'Name=tag:Name,Values=ANFW-VPC02-Private-B-10.2.22.102' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId'
+export ANFW_VPC02_21_101=$(aws ec2 describe-instances --region ${AWS_REGION} --filters 'Name=tag:Name,Values=ANFW-VPC02-Private-A-10.2.21.101' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId')
+export ANFW_VPC02_21_102=$(aws ec2 describe-instances --region ${AWS_REGION} --filters 'Name=tag:Name,Values=ANFW-VPC02-Private-A-10.2.21.102' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId')
+export ANFW_VPC02_22_101=$(aws ec2 describe-instances --region ${AWS_REGION} --filters 'Name=tag:Name,Values=ANFW-VPC02-Private-B-10.2.22.101' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId')
+export ANFW_VPC02_22_102=$(aws ec2 describe-instances --region ${AWS_REGION} --filters 'Name=tag:Name,Values=ANFW-VPC02-Private-B-10.2.22.102' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId')
 echo "export ANFW_VPC02_21_101=${ANFW_VPC02_21_101}"| tee -a ~/.bash_profile
 echo "export ANFW_VPC02_21_102=${ANFW_VPC02_21_102}"| tee -a ~/.bash_profile
 echo "export ANFW_VPC02_22_101=${ANFW_VPC02_22_101}"| tee -a ~/.bash_profile
 echo "export ANFW_VPC02_22_102=${ANFW_VPC02_22_102}"| tee -a ~/.bash_profile
 
-aws ec2 describe-instances --region ap-northeast-1 --filters 'Name=tag:Name,Values=ANFW-N2SVPC-Private-A-10.11.21.101' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId'
-aws ec2 describe-instances --region ap-northeast-1 --filters 'Name=tag:Name,Values=ANFW-N2SVPC-Private-A-10.11.21.102' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId'
-aws ec2 describe-instances --region ap-northeast-1 --filters 'Name=tag:Name,Values=ANFW-N2SVPC-Private-B-10.11.22.101' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId'
-aws ec2 describe-instances --region ap-northeast-1 --filters 'Name=tag:Name,Values=ANFW-N2SVPC-Private-B-10.11.22.102' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId'
-export ANFW_N2SVPC_21_101=$(aws ec2 describe-instances --region ap-northeast-1 --filters 'Name=tag:Name,Values=ANFW-N2SVPC-Private-A-10.11.21.101' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId')
-export ANFW_N2SVPC_21_102=$(aws ec2 describe-instances --region ap-northeast-1 --filters 'Name=tag:Name,Values=ANFW-N2SVPC-Private-A-10.11.21.102' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId')
-export ANFW_N2SVPC_22_101=$(aws ec2 describe-instances --region ap-northeast-1 --filters 'Name=tag:Name,Values=ANFW-N2SVPC-Private-B-10.11.22.101' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId')
-export ANFW_N2SVPC_22_102=$(aws ec2 describe-instances --region ap-northeast-1 --filters 'Name=tag:Name,Values=ANFW-N2SVPC-Private-B-10.11.22.102' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId')
+aws ec2 describe-instances --region ${AWS_REGION} --filters 'Name=tag:Name,Values=ANFW-N2SVPC-Private-A-10.11.21.101' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId'
+aws ec2 describe-instances --region ${AWS_REGION} --filters 'Name=tag:Name,Values=ANFW-N2SVPC-Private-A-10.11.21.102' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId'
+aws ec2 describe-instances --region ${AWS_REGION} --filters 'Name=tag:Name,Values=ANFW-N2SVPC-Private-B-10.11.22.101' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId'
+aws ec2 describe-instances --region ${AWS_REGION} --filters 'Name=tag:Name,Values=ANFW-N2SVPC-Private-B-10.11.22.102' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId'
+export ANFW_N2SVPC_21_101=$(aws ec2 describe-instances --region ${AWS_REGION} --filters 'Name=tag:Name,Values=ANFW-N2SVPC-Private-A-10.11.21.101' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId')
+export ANFW_N2SVPC_21_102=$(aws ec2 describe-instances --region ${AWS_REGION} --filters 'Name=tag:Name,Values=ANFW-N2SVPC-Private-A-10.11.21.102' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId')
+export ANFW_N2SVPC_22_101=$(aws ec2 describe-instances --region ${AWS_REGION} --filters 'Name=tag:Name,Values=ANFW-N2SVPC-Private-B-10.11.22.101' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId')
+export ANFW_N2SVPC_22_102=$(aws ec2 describe-instances --region ${AWS_REGION} --filters 'Name=tag:Name,Values=ANFW-N2SVPC-Private-B-10.11.22.102' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId')
 echo "export N2SVPC_21_101=${ANFW_N2SVPC_21_101}"| tee -a ~/.bash_profile
 echo "export N2SVPC_21_102=${ANFW_N2SVPC_21_102}"| tee -a ~/.bash_profile
 echo "export N2SVPC_22_101=${ANFW_N2SVPC_22_101}"| tee -a ~/.bash_profile
