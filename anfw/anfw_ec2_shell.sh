f6dce09b7606c58258c6cc2eaa5858bc419c8270
whchoi98 <whchoi98@gmail.com>
2024-01-27 12:19:20 +0900

    edit - anfw_ec2_shell.sh

--- anfw/anfw_ec2_shell.sh
+++ anfw/anfw_ec2_shell.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # command ./anfw_ec2_shell.sh
-
+echo "--------------------------"
+echo "VPC01 instance list"
+echo "--------------------------"
 aws ec2 describe-instances --region ${AWS_REGION} --filters 'Name=tag:Name,Values=ANFW-VPC01-Private-A-10.1.21.101' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId'
 aws ec2 describe-instances --region ${AWS_REGION} --filters 'Name=tag:Name,Values=ANFW-VPC01-Private-A-10.1.21.102' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId'
 aws ec2 describe-instances --region ${AWS_REGION} --filters 'Name=tag:Name,Values=ANFW-VPC01-Private-B-10.1.22.101' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId'
@@ -13,7 +15,9 @@ echo "export ANFW_VPC01_21_101=${ANFW_VPC01_21_101}"| tee -a ~/.bash_profile
 echo "export ANFW_VPC01_21_102=${ANFW_VPC01_21_102}"| tee -a ~/.bash_profile
 echo "export ANFW_VPC01_22_101=${ANFW_VPC01_22_101}"| tee -a ~/.bash_profile
 echo "export ANFW_VPC01_22_102=${ANFW_VPC01_22_102}"| tee -a ~/.bash_profile
-
+echo "-------------------------"
+echo "VPC02 instance list"
+echo "--------------------------"
 aws ec2 describe-instances --region ${AWS_REGION} --filters 'Name=tag:Name,Values=ANFW-VPC02-Private-A-10.2.21.101' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId'
 aws ec2 describe-instances --region ${AWS_REGION} --filters 'Name=tag:Name,Values=ANFW-VPC02-Private-A-10.2.21.102' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId'
 aws ec2 describe-instances --region ${AWS_REGION} --filters 'Name=tag:Name,Values=ANFW-VPC02-Private-B-10.2.22.101' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId'
@@ -26,7 +30,9 @@ echo "export ANFW_VPC02_21_101=${ANFW_VPC02_21_101}"| tee -a ~/.bash_profile
 echo "export ANFW_VPC02_21_102=${ANFW_VPC02_21_102}"| tee -a ~/.bash_profile
 echo "export ANFW_VPC02_22_101=${ANFW_VPC02_22_101}"| tee -a ~/.bash_profile
 echo "export ANFW_VPC02_22_102=${ANFW_VPC02_22_102}"| tee -a ~/.bash_profile
-
+echo "-------------------------"
+echo "N2SVPC instance list"
+echo "--------------------------"
 aws ec2 describe-instances --region ${AWS_REGION} --filters 'Name=tag:Name,Values=ANFW-N2SVPC-Private-A-10.11.21.101' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId'
 aws ec2 describe-instances --region ${AWS_REGION} --filters 'Name=tag:Name,Values=ANFW-N2SVPC-Private-A-10.11.21.102' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId'
 aws ec2 describe-instances --region ${AWS_REGION} --filters 'Name=tag:Name,Values=ANFW-N2SVPC-Private-B-10.11.22.101' 'Name=instance-state-name,Values=running' | jq -r '.Reservations[].Instances[].InstanceId'
@@ -39,3 +45,4 @@ echo "export N2SVPC_21_101=${ANFW_N2SVPC_21_101}"| tee -a ~/.bash_profile
 echo "export N2SVPC_21_102=${ANFW_N2SVPC_21_102}"| tee -a ~/.bash_profile
 echo "export N2SVPC_22_101=${ANFW_N2SVPC_22_101}"| tee -a ~/.bash_profile
 echo "export N2SVPC_22_102=${ANFW_N2SVPC_22_102}"| tee -a ~/.bash_profile
+echo "-------------------------"
