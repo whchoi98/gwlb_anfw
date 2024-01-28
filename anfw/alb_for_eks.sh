6bf81812e1d6449fd9e98f492827234330462c76
whchoi98 <whchoi98@gmail.com>
2024-01-28 12:49:24 +0900

    edit - alb_for_eks.sh

--- anfw/alb_for_eks.sh
+++ anfw/alb_for_eks.sh
@@ -23,7 +23,6 @@ aws elbv2 create-load-balancer --name ${ALB_FOR_EKS_NAME} \
 --subnets ${ANFW_N2SVPC_PublicSubnet01} ${ANFW_N2SVPC_PublicSubnet02} \
 --security-groups ${ANFW_N2SVPC_ALBSG}
 
-#sleep 120
 
 echo "-------------"
 echo "Status check - ALB for EKS Workload"
@@ -49,12 +48,22 @@ echo "register-targets - ALB for EKS Workload"
 echo "-------------"
 aws elbv2 register-targets \
     --target-group-arn ${ALB_FOR_EKS_TG_ARN} \
-    --targets Id=${NLB_PRIVATE_A_IP} Id=${NLB_PRIVATE_B_IP}
+    --targets Id=${NLB_PRIVATE_A_IP},AvailabilityZone=all Id=${NLB_PRIVATE_B_IP},AvailabilityZone=all
 
 echo "-------------"
 echo "create-listener - ALB for EKS Workload"
 echo "-------------"
-aws elbv2 create-listener --load-balancer-arn $ALB_FOR_EKS_ARN --protocol HTTP --port 80  \
+aws elbv2 create-listener --load-balancer-arn $ALB_FOR_EKS_ARN  --protocol HTTP --port 80  \
 --default-actions Type=forward,TargetGroupArn=${ALB_FOR_EKS_TG_ARN}
+echo "export ALB_FOR_EKS_LISTENER_ARN=$(aws elbv2 describe-listeners --load-balancer-arn $ALB_FOR_EKS_ARN | jq -r '.Listeners[].ListenerArn')" | tee -a ~/.bash_profile
+source ~/.bash_profile
 
-aws elbv2 describe-load-balancers  --name ${ALB_FOR_EKS_NAME} | jq -r '.LoadBalancers[].DNSName'
+echo "-------------"
+echo "EKS-Sample-APP-URL ADDRESS"
+echo "-------------"
+echo "EKS-Sample-APP-URL = http://$(aws elbv2 describe-load-balancers  --name ${ALB_FOR_EKS_NAME} | jq -r '.LoadBalancers[].DNSName')"
+
+###How to delete elbv2###
+#aws elbv2 delete-listener --listener-arn $ALB_FOR_EKS_LISTENER_ARN
+#aws elbv2 delete-target-group --target-group-arn $ALB_FOR_EKS_TG_ARN
+#aws elbv2 delete-load-balancer --load-balancer-arn $ALB_FOR_EKS_ARN
